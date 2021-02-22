d4b10411ea73837cd0e6e59d276ec2f5d0b34f41
Sean Tibor <sean.tibor@pinecrest.edu>
2021-02-22 16:06:43 -0500

    complete working solution

--- main.py
+++ main.py
@@ -2,43 +2,69 @@
 Welcome to the ALAF program. An awesome program for awesome friends.
 '''
 import json
+from tabulate import tabulate
 
 friends_file = "alaf.json"
 
 def welcome():
     print("Welcome to ALAF!")
 
-def load_friends():
+def load_friends(friends_file=friends_file):
     try:
         with open(friends_file) as f:
             friends = json.load(f)
     # create an empty list if the file is not there
     except FileNotFoundError:
         friends = []
+    except json.JSONDecodeError:
+        friends = []
+        print(f"Could not load friends from {friends_file}")
 
     return friends
 
 def save_friends(friends):
+    if not friends:
+        print("The friends list is empty. Not saving it!")
+        return
     with open(friends_file, mode="w") as f:
         # write the friends list to the file object f
         json.dump(friends, f)
+    print(f"{len(friends)} written to {friends_file}.")
 
 def instructions():
     print("<INSTRUCTIONS GO HERE> (Remember to make them awesome)")
 
 def menu():
-    pass
+    menu_text='''==== Main Menu ====
+(L)ist Friends
+(A)dd Friend
+(C)lear List
+(Q)uit
+
+Your choice? 
+'''
+    choice = ''
+    options = set('lacq')
+    while True:
+        choice = input(menu_text).strip().lower()
+        if choice in options:
+            return choice
+        print("I didn't understand. Please try again.\n")
 
 def list_friends(friends):
     # use tabulate later, just print for now
-    print(friends)
+    print(tabulate(friends, headers='keys', tablefmt='fancy_grid'))
 
 def add_friend():
+    questions = ["Name", "Phone Number", "Email Address", "Instagram", "Social Security Number"]
     friend = {}
-    name = input("What is your friend's name? ")
-    friend['Name'] = name
-    return friend
 
+    print("Enter the information for the new friend")
+    print("----------------------------------------")
+
+    for question in questions:
+        friend[question] = input(f"{question.title()}: ")
+    return friend
 
 def exit_list():
     pass
@@ -48,10 +74,21 @@ def main():
     welcome()
     instructions()
     friends = load_friends()
-    new_friend = add_friend()
-    friends.append(new_friend)
-    list_friends(friends)
+    print(f"{len(friends)} friends loaded from disk.")
+    choice = ''
+    while choice != 'q':
+        choice = menu()
+        if choice == 'l':
+            list_friends(friends)
+        elif choice == 'a':
+            new_friend = add_friend()
+            friends.append(new_friend)
+            print(f'{new_friend["Name"]} added to list')
+        elif choice == 'c':
+            friends = []
+    
     save_friends(friends)
+    print("Thank you for using the friends list!")
 
 ''' The better way, but optional
 if __name__ == "__main__":
